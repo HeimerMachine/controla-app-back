27bf5504d11ca254be3209cfb093e889e4a80263
ThulioBezerra <thulioalves20@gmail.com>
2025-05-22 15:08:16 -0300

    remove: remove try...catch from the requests to use errorhandle.

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,33 +1,29 @@
-import jsonwtoken, { JsonWebTokenError } from 'jsonwebtoken';
-import { NextFunction, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import jsonwtoken, { JsonWebTokenError } from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+import { AuthorizationTokenNotFoundError } from "@helpers/user-errors/authorizationTokenNotFoundError";
+import { InvalidToken } from "@helpers/user-errors/invalidToken";
 
 dotenv.config();
 
-
 async function authMiddleware(req: Request, res: Response, next: NextFunction) {
-  const token = req.headers['authorization']?.split(' ')[1];
+  const token = req.headers["authorization"]?.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ message: 'Token não encontrado' });
+    throw new AuthorizationTokenNotFoundError();
   }
 
-  try {
-    const decoded = jsonwtoken.verify(token, dotenv.config().parsed?.JWT_SECRET as string);
-    res.locals.user = {
-        id: (decoded as { id: string }).id,
-    };
-    next();
-  } catch (error: JsonWebTokenError | unknown) {
-    if (error instanceof JsonWebTokenError) {
-      return res.status(401).json({ message: 'Token inválido' });
-    }
-    if (error instanceof Error) {
-      return res.status(401).json({ message: error.message });
-    }
-    if(error)
-    return res.status(500).json({ message: 'Erro interno no servidor' });
+  const decoded = jsonwtoken.verify(
+    token,
+    dotenv.config().parsed?.JWT_SECRET as string
+  );
+  if(decoded instanceof JsonWebTokenError) {
+    throw new InvalidToken();
   }
+  res.locals.user = {
+    id: (decoded as { id: string }).id,
+  };
+  next();
 }
 
 export default authMiddleware;
--- src/services/user/registerService.ts
+++ src/services/user/registerService.ts
@@ -1,8 +1,7 @@
 import { User } from "../../entities/User";
 import { UserRepository } from "../../repositories/UserRepository";
 import EncryptPasswordService from "./encryptPassword";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
-import { UserAlreadyExistsError } from "@helpers/user-errors/userAlreadyExistsError";
+
 
 export class RegisterService {
   constructor(private readonly userRepository: UserRepository) {
@@ -13,16 +12,7 @@ export class RegisterService {
     user.password = await this.encryptPasswordService.encryptPassword(
       user.password
     );
-    try {
-      const userCreated = await this.userRepository.create(user);
-      return userCreated;
-    } catch (error: PrismaClientKnownRequestError | unknown) {
-      if (error instanceof PrismaClientKnownRequestError) {
-        if (error.code === "P2002") {
-          throw new UserAlreadyExistsError();
-        }
-      }
-      throw new Error(`Internal server error`);
-    }
+    const userCreated = await this.userRepository.create(user);
+    return userCreated;
   }
 }
