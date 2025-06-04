c472146af22cf0e03963248c86881d2399bbd188
Samuel-Marcos-Holanda <samuel.holanda@aluno.uepb.edu.br>
2025-06-03 22:57:04 -0300

    feat: adding first implementation of income classes

---
+++ src/entities/Finance.ts
@@ -0,0 +1,7 @@
+export interface Finance {
+    id: string;
+    userId: string;
+    amount: number;
+    description: string;
+    date: Date;
+}
---
+++ src/entities/Income.ts
@@ -0,0 +1,23 @@
+import { Finance } from "./Finance";
+
+export class Income implements Finance {
+    id: string;
+    userId: string;
+    amount: number;
+    description: string;
+    date: Date;
+
+    constructor(
+    id: string,
+        userId: string,
+        amount: number,
+        description: string,
+        date: Date
+    ) {
+        this.id = id;
+        this.userId = userId;
+        this.amount = amount;
+        this.description = description;
+        this.date = date;
+    }
+}
